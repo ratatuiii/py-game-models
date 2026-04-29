fcc0e037f930bd2c81ce601e6852aace1fa6612b
naomi <orest.chupa@gmail.com>
2026-04-29 20:40:58 +0300

    Fix all names now unique

--- db/models.py
+++ db/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Race(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
     description = models.TextField(blank=True)
 
 
@@ -13,12 +13,12 @@ class Skill(models.Model):
 
 
 class Guild(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
     description = models.TextField(null=True, blank=True)
 
 
 class Player(models.Model):
-    nickname = models.CharField(max_length=255)
+    nickname = models.CharField(max_length=255, unique=True)
     email = models.EmailField(max_length=255)
     bio = models.CharField(max_length=255)
     race = models.ForeignKey(Race, on_delete=models.CASCADE)
